a871a85495824970324c2098b31e20916af2a2a5
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-06-19 13:21:18 +0200

    Making stateless serializer and deserializer methods static

--- test/framer.js
+++ test/framer.js
@@ -13,35 +13,35 @@ var frame_types = {
 
 describe('Framer', function() {
   describe('Serializer', function() {
-    describe('.prototype.commonHeader({ length, type, flags, stream })', function() {
+    describe('static method .commonHeader({ length, type, flags, stream })', function() {
       it('should return the appropriate 8 byte header buffer')
     })
 
     for (var type in frame_types) {
-      describe('.prototype[\'' + type + '\']({ ' + frame_types[type].join(', ') + ' })', function() {
+      describe('static method [\'' + type + '\']({ ' + frame_types[type].join(', ') + ' })', function() {
         it('should return a ' + type + ' type payload buffer')
       })
     }
   })
 
   describe('Deserializer', function() {
-    describe('.prototype.commonHeader(buffer)', function() {
+    describe('static method .commonHeader(buffer)', function() {
       it('should return the appropriate header object')
     })
 
     for (var type in frame_types) {
-      describe('.prototype[\'' + type + '\'](buffer)', function() {
-        it('should return an object with these properties: ' + frame_types[type].join(', '))
+      describe('static method [\'' + type + '\'](buffer)', function() {
+        it('should return the parsed frame object with these properties: ' + frame_types[type].join(', '))
       })
     }
   })
 
   describe('invariants', function() {
-    describe('frame === deserializer.commonHeader(serializer.commonHeader(frame))', function() {
+    describe('frame === Deserializer.commonHeader(Serializer.commonHeader(frame))', function() {
       it('should always be true for well formed frame object')
     })
 
-    describe('buffer === serializer.commonHeader(deserializer.commonHeader(buffer))', function() {
+    describe('buffer === Serializer.commonHeader(Deserializer.commonHeader(buffer))', function() {
       it('should always be true for well formed frame buffers')
     })
   })
