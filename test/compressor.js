df3bd144d8d77394fbf89ee06290bd0dba036bf7
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-06-26 13:01:26 +0200

    Small modification to the compressor internal API

--- test/compressor.js
+++ test/compressor.js
@@ -7,32 +7,45 @@ var compressor = require('../lib/compressor')
 var test_integers = [{
   N: 5,
   I: 10,
-  bytes: [10]
+  buffer: new Buffer([10])
 }, {
   N: 5,
   I: 1337,
-  bytes: [31, 128 + 26, 10]
+  buffer: new Buffer([31, 128 + 26, 10])
 }]
+
+// Concatenate buffers into a new buffer
+function concat(buffers) {
+  var size = 0
+  for (var i = 0; i < buffers.length; i++) size += buffers[i].length
+
+  var concatenated = new Buffer(size)
+  for (var cursor = 0, j = 0; j < buffers.length; cursor += buffers[j].length, j++) {
+    buffers[j].copy(concatenated, cursor)
+  }
+
+  return concatenated
+}
 
 describe('Compressor', function() {
   describe('static function integer(I, N)', function() {
-    it('should return an array of bytes that represent the N-prefix coded I value', function() {
+    it('should return an array of buffers that represent the N-prefix coded I value', function() {
       for (var i = 0; i < test_integers.length; i++) {
         var test = test_integers[i]
-        expect(Compressor.integer(test.I, test.N)).to.deep.equal(test.bytes)
+        expect(concat(Compressor.integer(test.I, test.N))).to.deep.equal(test.buffer)
       }
     })
   })
 })
 
 describe('Decompressor', function() {
-  describe('static function integer(bytes, N)', function() {
-    it('should return { value: the parsed N-prefix coded number, length: number of bytes processed }', function() {
+  describe('static function integer(buffer, N)', function() {
+    it('should return the parsed N-prefix coded number and increase the cursor property of buffer', function() {
       for (var i = 0; i < test_integers.length; i++) {
         var test = test_integers[i]
-        var result = Decompressor.integer(test.bytes, test.N)
-        expect(result.value).to.equal(test.I)
-        expect(result.length).to.equal(test.bytes.length)
+        test.buffer.cursor = 0
+        expect(Decompressor.integer(test.buffer, test.N)).to.equal(test.I)
+        expect(test.buffer.cursor).to.equal(test.buffer.length)
       }
     })
   })
