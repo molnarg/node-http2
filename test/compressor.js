11ed64592897ff98f966b9700d40804796662cbd
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-07-03 11:24:37 +0200

    Reverting to using strings instead of buffers for storing strings internally in compressor.

--- test/compressor.js
+++ test/compressor.js
@@ -25,31 +25,31 @@ var test_integers = [{
 }];
 
 var test_strings = [{
-  string: new Buffer('abcdefghij', 'utf-8'),
+  string: 'abcdefghij',
   buffer: new Buffer('0A6162636465666768696A', 'hex')
 }, {
-  string: new Buffer('éáűőúöüó€', 'utf-8'),
+  string: 'éáűőúöüó€',
   buffer: new Buffer('13C3A9C3A1C5B1C591C3BAC3B6C3BCC3B3E282AC', 'hex')
 }];
 
 var test_headers = [{
   header: {
     name: 3,
-    value: new Buffer('/my-example/index.html', 'utf-8'),
+    value: '/my-example/index.html',
     indexing: true
   },
   buffer: new Buffer('44' + '162F6D792D6578616D706C652F696E6465782E68746D6C', 'hex')
 }, {
   header: {
     name: 12,
-    value: new Buffer('my-user-agent', 'utf-8'),
+    value: 'my-user-agent',
     indexing: true
   },
   buffer: new Buffer('4D' + '0D6D792D757365722D6167656E74', 'hex')
 }, {
   header: {
-    name: new Buffer('x-my-header', 'utf-8'),
-    value: new Buffer('first', 'utf-8'),
+    name: 'x-my-header',
+    value: 'first',
     indexing: true
   },
   buffer: new Buffer('40' + '0B782D6D792D686561646572' + '056669727374', 'hex')
@@ -70,7 +70,7 @@ var test_headers = [{
 }, {
   header: {
     name: 3,
-    value: new Buffer('/my-example/resources/script.js', 'utf-8'),
+    value: '/my-example/resources/script.js',
     indexing: true,
     substitution: 38
   },
@@ -78,7 +78,7 @@ var test_headers = [{
 }, {
   header: {
     name: 40,
-    value: new Buffer('second', 'utf-8'),
+    value: 'second',
     indexing: true
   },
   buffer: new Buffer('5F0A' + '067365636F6E64', 'hex')
@@ -100,10 +100,10 @@ describe('compressor.js', function() {
 
   describe('Compressor', function() {
     describe('static method .integer(I, N)', function() {
-      it('should return an array of buffers that represent the N-prefix coded I value', function() {
-        for (var i = 0; i < test_integers.length; i++) {
-          var test = test_integers[i];
-          expect(concat(Compressor.integer(test.I, test.N))).to.deep.equal(test.buffer);
+      it('should return an array of buffers that represent the encoded form of the string str', function() {
+        for (var i = 0; i < test_strings.length; i++) {
+          var test = test_strings[i];
+          expect(concat(Compressor.string(test.string))).to.deep.equal(test.buffer);
         }
       });
     });
@@ -137,11 +137,11 @@ describe('compressor.js', function() {
       });
     });
     describe('static method .string(buffer)', function() {
-      it('should return the parsed string buffer and increase the cursor property of buffer', function() {
+      it('should return the parsed string and increase the cursor property of buffer', function() {
         for (var i = 0; i < test_strings.length; i++) {
           var test = test_strings[i];
           test.buffer.cursor = 0;
-          expect(Decompressor.string(test.buffer)).to.deep.equal(test.string);
+          expect(Decompressor.string(test.buffer)).to.equal(test.string);
           expect(test.buffer.cursor).to.equal(test.buffer.length);
         }
       });
