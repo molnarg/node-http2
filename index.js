323cb81b2c658f4f2bf6596c046c555b1aaf35c4
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-07-09 10:30:59 +0200

    Adding HTTP and HTTPS API stubs.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-exports.framer     = require('./lib/framer');
 exports.compressor = require('./lib/compressor');
 exports.connection = require('./lib/connection');
+exports.framer     = require('./lib/framer');
+exports.http       = require('./lib/http');
+exports.https      = require('./lib/https');
+exports.stream     = require('./lib/stream');
+exports.utils      = require('./lib/utils');
