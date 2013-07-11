23c58fa8e8bc007a4f1f8ad1253148f4535d0407
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-07-11 14:39:31 +0200

    Correcting stream tests.

--- test/stream.js
+++ test/stream.js
@@ -6,65 +6,94 @@ var log = process.env.DEBUG ? require('bunyan').createLogger({ name: 'http2', le
                             : undefined;
 
 // Execute a list of commands and assertions
-function execute_sequence(sequence) {
+function execute_sequence(sequence, done) {
   var stream = new Stream(log);
 
   var outgoing_frames = [];
-  stream.upstream.on('data', outgoing_frames.push.bind(outgoing_frames));
+  stream.upstream.on('sending', outgoing_frames.push.bind(outgoing_frames));
 
   var emit = stream.emit, events = [];
   stream.emit = function(name, data) {
-    events.push({ name: name, data: data });
+    if (name === 'state' || name === 'error' || name === 'window_update') {
+      events.push({ name: name, data: data });
+    }
     return emit.apply(this, arguments);
   };
 
+  var commands = [], checks = [];
   sequence.forEach(function(step) {
-    if ('state' in step) {
-      expect(stream).to.have.property('state', step.state);
-
-    } else if ('outgoing' in step) {
-      expect(outgoing_frames.shift()).to.deep.equal(step.outgoing);
-
-    } else if ('event' in step) {
-      expect(events.shift()).to.deep.equal(step.event);
-
-    } else if ('method' in step) {
-      stream[step.method.name].apply(stream, step.method.arguments);
-
-    } else if ('incoming' in step) {
-      stream.upstream.push(step.incoming);
-
+    if ('method' in step || 'incoming' in step || 'wait' in step) {
+      commands.push(step);
     } else {
-      throw new Error('Invalid step');
+      checks.push(step);
     }
   });
+
+  function execute(callback) {
+    var command = commands.shift();
+    if (command) {
+      if ('method' in command) {
+        stream[command.method.name].apply(stream, command.method.arguments);
+        execute(callback);
+      } else if ('incoming' in command) {
+        stream.upstream.write(command.incoming);
+        execute(callback);
+      } else if ('wait' in command) {
+        setTimeout(execute.bind(null, callback), command.wait);
+      } else {
+        throw new Error('Invalid command', command);
+      }
+    } else {
+      callback();
+    }
+  }
+
+  function check() {
+    checks.forEach(function(check) {
+      if ('outgoing' in check) {
+        console.log(outgoing_frames[0], check.outgoing);
+        expect(outgoing_frames.shift()).to.deep.equal(check.outgoing);
+      } else if ('event' in check) {
+        expect(events.shift()).to.deep.equal(check.event);
+      } else {
+        throw new Error('Invalid check', check);
+      }
+    });
+    done();
+  }
+
+  execute(check);
 }
 
 describe('stream.js', function() {
   describe('Stream class', function() {
-    it('should go over the appropriate states in the simplest client request scenario', function() {
+    it.only('should go over the appropriate states in the simplest client request scenario', function(done) {
       execute_sequence([
         { method  : { name: 'open', arguments: [{ ':path': '/' }] } },
-        { outgoing: { type: 'HEADERS', flags: { END_STREAM: true  }, headers: { ':path': '/' } } },
+        { method  : { name: 'end', arguments: [] } },
+        { outgoing: { type: 'HEADERS', flags: { END_STREAM: true  }, headers: { ':path': '/' }, priority: undefined } },
         { event   : { name: 'state', data: 'OPEN' } },
         { event   : { name: 'state', data: 'HALF_CLOSED_LOCAL' } },
 
+        { wait    : 10 },
         { incoming: { type: 'HEADERS', flags: { END_STREAM: false }, headers: { ':status': 200 } } },
         { incoming: { type: 'DATA'   , flags: { END_STREAM: true  }, data: new Buffer(5) } },
         { event   : { name: 'state', data: 'CLOSED' } }
-      ]);
+      ], done);
     });
-    it('should go over the appropriate states in a server push scenario', function() {
+    it('should go over the appropriate states in a server push scenario', function(done) {
       execute_sequence([
         { incoming: { type: 'PUSH_PROMISE', flags: { END_STREAM: false }, headers: { ':path': '/' } } },
         { event   : { name: 'state', data: 'RESERVED_REMOTE' } },
 
+        { wait    : 10 },
         { incoming: { type: 'HEADERS'     , flags: { END_STREAM: false }, headers: { ':status': 200 } } },
         { event   : { name: 'state', data: 'HALF_CLOSED_LOCAL' } },
 
+        { wait    : 10 },
         { incoming: { type: 'DATA'        , flags: { END_STREAM: true  }, data: new Buffer(5) } },
         { event   : { name: 'state', data: 'CLOSED' } }
-      ]);
+      ], done);
     });
   });
 });
