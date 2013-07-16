7e293a23597d5e63fe720df20f7cb222ca7bf07d
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-07-16 19:07:39 +0200

    One basic test for endpoint.

---
+++ test/endpoint.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+
+var endpoint = require('../lib/endpoint');
+var Endpoint = endpoint.Endpoint;
+var Server   = endpoint.Server;
+var Client   = endpoint.Client;
+
+var log = process.env.DEBUG ? require('bunyan').createLogger({ name: 'http2', level: 'trace' })
+                            : undefined;
+
+describe('endpoint.js', function() {
+  describe('scenario', function() {
+    describe('connection setup', function() {
+      it('should work as expected', function(done) {
+        var c = new Client();
+        var s = new Server();
+
+        c.pipe(s).pipe(c);
+
+        setTimeout(function() {
+          // If there are no exception until this, then we're done
+          done();
+        }, 10);
+      });
+    });
+  });
+});
