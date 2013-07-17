5ee44c4b41b18e301ba2460420d8d5e8e728a097
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-07-17 12:17:07 +0200

    Updating and deocumenting index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-exports.compressor = require('./lib/compressor');
-exports.connection = require('./lib/connection');
-exports.framer     = require('./lib/framer');
-exports.http       = require('./lib/http');
-exports.https      = require('./lib/https');
-exports.stream     = require('./lib/stream');
-exports.utils      = require('./lib/utils');
