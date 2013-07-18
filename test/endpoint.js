db8837c1d53564446b6216a0c21129cb0d7ccc79
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-07-18 11:53:43 +0200

    Mergin Client and Server class back into Endpoint

--- test/endpoint.js
+++ test/endpoint.js
@@ -1,17 +1,17 @@
 var expect = require('chai').expect;
+var log_root = require('../lib/logging').root;
 
 var endpoint = require('../lib/endpoint');
 var Endpoint = endpoint.Endpoint;
-var Server   = endpoint.Server;
-var Client   = endpoint.Client;
 
 describe('endpoint.js', function() {
   describe('scenario', function() {
     describe('connection setup', function() {
       it('should work as expected', function(done) {
-        var c = new Client();
-        var s = new Server();
+        var c = new Endpoint('CLIENT', {}, log_root.child({ role: 'client' }));
+        var s = new Endpoint('SERVER', {}, log_root.child({ role: 'server' }));
 
+        log_root.debug('Test initialization over, starting piping.');
         c.pipe(s).pipe(c);
 
         setTimeout(function() {
