88fcb5670ab10dab66658ca956eb9d2a3d7dd6a4
Gábor Molnár <gabor@molnar.es>
2013-08-02 16:00:47 +0200

    Example server: code cleanup and using TLS by default.

--- example/server.js
+++ example/server.js
@@ -1,25 +1,24 @@
 var fs = require('fs');
-var net = require('net');
 var path = require('path');
 var http2 = require('../lib/index');
-var Endpoint = http2.endpoint.Endpoint;
 
 var settings = {
-  SETTINGS_MAX_CONCURRENT_STREAMS: 1,
+  SETTINGS_MAX_CONCURRENT_STREAMS: 10,
   SETTINGS_INITIAL_WINDOW_SIZE: 100000
 };
 
 var server = http2.http.createServer({
-  plain: true,
+  key: fs.readFileSync(path.join(__dirname, './localhost.key')),
+  cert: fs.readFileSync(path.join(__dirname, '/localhost.crt')),
   settings: settings
 }, function(request, response) {
   var filename = path.join(__dirname, request.url);
   console.error('Incoming request:', request.url, '(' + filename + ')');
 
-  if (fs.existsSync(filename)) {
+  if (fs.existsSync(filename) && fs.statSync(filename).isFile()) {
     console.error('Reading file from disk.');
-    response.writeHead('200');
     var filestream = fs.createReadStream(filename);
+    response.writeHead('200');
     filestream.pipe(response);
 
   } else {
