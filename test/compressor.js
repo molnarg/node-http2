b88195992184990dd0b7a22ec6823121cb2aafc9
Gábor Molnár <gabor@molnar.es>
2013-08-12 21:17:38 +0200

    Utils: inlining utility function into corresponding files (that means some code duplication in tests)

--- test/compressor.js
+++ test/compressor.js
@@ -1,10 +1,24 @@
 var expect = require('chai').expect;
-var concat = require('../lib/utils').concat;
 
 var compressor = require('../lib/compressor');
 var CompressionContext = compressor.CompressionContext;
 var Compressor = compressor.Compressor;
 var Decompressor = compressor.Decompressor;
+
+// Concatenate an array of buffers into a new buffer
+function concat(buffers) {
+  var size = 0;
+  for (var i = 0; i < buffers.length; i++) {
+    size += buffers[i].length;
+  }
+
+  var concatenated = new Buffer(size);
+  for (var cursor = 0, j = 0; j < buffers.length; cursor += buffers[j].length, j++) {
+    buffers[j].copy(concatenated, cursor);
+  }
+
+  return concatenated;
+}
 
 var test_integers = [{
   N: 5,
--- test/framer.js
+++ test/framer.js
@@ -1,9 +1,23 @@
 var expect = require('chai').expect;
-var concat = require('../lib/utils').concat;
 
 var framer = require('../lib/framer');
 var Serializer = framer.Serializer;
 var Deserializer = framer.Deserializer;
+
+// Concatenate an array of buffers into a new buffer
+function concat(buffers) {
+  var size = 0;
+  for (var i = 0; i < buffers.length; i++) {
+    size += buffers[i].length;
+  }
+
+  var concatenated = new Buffer(size);
+  for (var cursor = 0, j = 0; j < buffers.length; cursor += buffers[j].length, j++) {
+    buffers[j].copy(concatenated, cursor);
+  }
+
+  return concatenated;
+}
 
 var frame_types = {
   DATA:          ['data'],
