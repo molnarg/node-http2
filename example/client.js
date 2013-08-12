92ed8b815aa61901374f8c9ef9811f8075ba5f0a
Gábor Molnár <gabor@molnar.es>
2013-08-12 11:38:59 +0200

    Connection: do not require SETTINGS_MAX_CONCURRENT_STREAMS and SETTINGS_INITIAL_WINDOW_SIZE all the time.

--- example/client.js
+++ example/client.js
@@ -2,11 +2,6 @@ var parse_url = require('url').parse;
 var path = require('path');
 var http2 = require('..');
 
-var settings = {
-  SETTINGS_MAX_CONCURRENT_STREAMS: 1,
-  SETTINGS_INITIAL_WINDOW_SIZE: 100000
-};
-
 var url = parse_url(process.argv.pop());
 
 var request = http2.request({
