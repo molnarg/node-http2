0e1b974645cf88e2d9d901e58f720db495691f15
Gábor Molnár <gabor@molnar.es>
2013-08-13 14:41:36 +0200

    Connection test: new test case for server push

--- test/connection.js
+++ test/connection.js
@@ -3,6 +3,16 @@ var log_root = require('../lib/logging').root;
 
 var Connection = require('../lib/connection').Connection;
 
+function callNTimes(limit, done) {
+  var i = 0;
+  return function() {
+    i += 1;
+    if (i === limit) {
+      done();
+    }
+  };
+}
+
 var settings = {
   SETTINGS_MAX_CONCURRENT_STREAMS: 100,
   SETTINGS_INITIAL_WINDOW_SIZE: 100000
@@ -56,20 +66,64 @@ describe('connection.js', function() {
         client_stream.end(request_data);
 
         // Waiting for answer
-        var headers_arrived = false;
-        var data_arrived = false;
+        done = callNTimes(2, done);
         client_stream.on('headers', function(headers) {
           expect(headers).to.deep.equal(response_headers);
-          headers_arrived = true;
-        });
-        client_stream.on('data', function(chunk) {
-          expect(chunk).to.deep.equal(response_data);
-          data_arrived = true;
-        });
-        client_stream.on('end', function() {
-          expect(headers_arrived).to.equal(true);
-          expect(data_arrived).to.equal(true);
           done();
+        });
+        client_stream.on('readable', function() {
+          expect(client_stream.read()).to.deep.equal(response_data);
+          done();
+        });
+      });
+    });
+    describe('server push', function() {
+      it('should work as expected', function(done) {
+        var c = new Connection(1, settings, log_root.child({ role: 'client' }));
+        var s = new Connection(2, settings, log_root.child({ role: 'server' }));
+
+        c.pipe(s).pipe(c);
+
+        var request_headers = { ':method': 'get', ':path': '/' };
+        var response_headers = { ':status': '200' };
+        var push_request_headers = { ':method': 'get', ':path': '/x' };
+        var push_response_headers = { ':status': '200' };
+        var response_content = new Buffer(10);
+        var push_content = new Buffer(10);
+
+        done = callNTimes(4, done);
+
+        s.on('stream', function(response) {
+          response.headers(response_headers);
+
+          var pushed = this.createStream();
+          response.promise(pushed, push_request_headers);
+          pushed.headers(push_response_headers);
+          pushed.write(push_content);
+
+          response.write(response_content);
+        });
+
+        var request = c.createStream();
+        request.headers(request_headers);
+        request.on('headers', function(headers) {
+          expect(headers).to.deep.equal(response_headers);
+          done();
+        });
+        request.on('readable', function() {
+          expect(request.read()).to.deep.equal(response_content);
+          done();
+        });
+        request.on('promise', function(headers, pushed) {
+          expect(headers).to.deep.equal(push_request_headers);
+          pushed.on('headers', function(headers) {
+            expect(headers).to.deep.equal(response_headers);
+            done();
+          });
+          pushed.on('readable', function() {
+            expect(pushed.read()).to.deep.equal(push_content);
+            done();
+          });
         });
       });
     });
