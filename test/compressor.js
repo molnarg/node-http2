a1d42f8032a877e81bd6bf1c530896b259579d87
Gábor Molnár <gabor@molnar.es>
2013-11-09 21:06:50 +0100

    Compression: adding a Huffman encoder/decoder

--- test/compressor.js
+++ test/compressor.js
@@ -3,6 +3,7 @@ var util = require('./util');
 
 var compressor = require('../lib/compressor');
 var HeaderTable = compressor.HeaderTable;
+var HuffmanTable = compressor.HuffmanTable;
 var HeaderSetCompressor = compressor.HeaderSetCompressor;
 var HeaderSetDecompressor = compressor.HeaderSetDecompressor;
 var Compressor = compressor.Compressor;
@@ -33,6 +34,74 @@ var test_strings = [{
   string: 'éáűőúöüó€',
   buffer: new Buffer('13C3A9C3A1C5B1C591C3BAC3B6C3BCC3B3E282AC', 'hex')
 }];
+
+test_huffman_request = {
+  'GET': 'f77778ff',
+  'http': 'ce3177',
+  '/': '0f',
+  'www.foo.com': 'db6d898b5a44b74f',
+  'https': 'ce31743f',
+  'www.bar.com': 'db6d897a1e44b74f',
+  'no-cache': '63654a1398ff',
+  '/custom-path.css': '04eb08b7495c88e644c21f',
+  'custom-key': '4eb08b749790fa7f',
+  'custom-value': '4eb08b74979a17a8ff'
+};
+
+test_huffman_response = {
+  '302': '409f',
+  'private': 'c31b39bf387f',
+  'Mon, 21 OCt 2013 20:13:21 GMT': 'a2fba20320f2ebcc0c490062d2434c827a1d',
+  ': https://www.bar.com': '6871cf3c326ebd7e9e9e926e7e32557dbf',
+  '200': '311f',
+  'Mon, 21 OCt 2013 20:13:22 GMT': 'a2fba20320f2ebcc0c490062d2434cc27a1d',
+  'https://www.bar.com': 'e39e7864dd7afd3d3d24dcfc64aafb7f',
+  'gzip': 'e1fbb30f',
+  'foo=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
+AAAAAAAAAAAAAAAAAAAAAAAAAALASDJKHQKBZXOQWEOPIUAXQWEOIUAXLJKHQWOEIUAL\
+QWEOIUAXLQEUAXLLKJASDQWEOUIAXN1234LASDJKHQKBZXOQWEOPIUAXQWEOIUAXLJKH\
+QWOEIUALQWEOIUAXLQEUAXLLKJASDQWEOUIAXN1234LASDJKHQKBZXOQWEOPIUAXQWEO\
+IUAXLJKHQWOEIUALQWEOIUAXLQEUAXLLKJASDQWEOUIAXN1234LASDJKHQKBZXOQWEOP\
+IUAXQWEOIUAXLJKHQWOEIUALQWEOIUAXLQEUAXLLKJASDQWEOUIAXN1234ZZZZZZZZZZ\
+ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ1234 m\
+ax-age=3600; version=1': 'df7dfb36eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76\
+eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76eddbb\
+76eddbb76eddbb7e3b69ecf0fe7e1fd7f3d5fe7f7e5fd79f6f97cbbfe9b7fbfebcfb\
+7cbbfe9b7fbf8f87f3f0febcfcbb7bfe9b7e3fd79f6f977fd36ff7f1febb7e9b7fbf\
+8fc7f9f0db4f67f5e7dbe5f4efdbfdf891a13f1db4f6787f3f0febf9eaff3fbf2feb\
+cfb7cbe5dff4dbfdff5e7dbe5dff4dbfdfc7c3f9f87f5e7e5dbdff4dbf1febcfb7cb\
+bfe9b7fbf8ff5dbf4dbfdfc7e3fcf86da7b3faf3edf2fa77edfefc48d09f8eda7b3c\
+3f9f87f5fcf57f9fdf97f5e7dbe5f2effa6dfeffaf3edf2effa6dfefe3e1fcfc3faf\
+3f2edeffa6df8ff5e7dbe5dff4dbfdfc7faedfa6dfefe3f1fe7c36d3d9fd79f6f97d\
+3bf6ff7e24684fc76d3d9e1fcfc3fafe7abfcfefcbfaf3edf2f977fd36ff7fd79f6f\
+977fd36ff7f1f0fe7e1fd79f976f7fd36fc7faf3edf2effa6dfefe3fd76fd36ff7f1\
+f8ff3e1b69ecfebcfb7cbe9dfb7fbf123427fcff3fcff3fcff3fcff3fcff3fcff3fc\
+ff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3\
+fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcf\
+f3fcff3fcff08d090b5fd237f086c44a23ef0e70c72b2fbb617f',
+  'foo=ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ\
+ZZZZZZZZZZZZZZZZZZZZZZZZZZLASDJKHQKBZXOQWEOPIUAXQWEOIUAXLJKHQWOEIUAL\
+QWEOIUAXLQEUAXLLKJASDQWEOUIAXN1234LASDJKHQKBZXOQWEOPIUAXQWEOIUAXLJKH\
+QWOEIUALQWEOIUAXLQEUAXLLKJASDQWEOUIAXN1234LASDJKHQKBZXOQWEOPIUAXQWEO\
+IUAXLJKHQWOEIUALQWEOIUAXLQEUAXLLKJASDQWEOUIAXN1234LASDJKHQKBZXOQWEOP\
+IUAXQWEOIUAXLJKHQWOEIUALQWEOIUAXLQEUAXLLKJASDQWEOUIAXN1234AAAAAAAAAA\
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1234 m\
+ax-age=3600; version=1': 'df7dfb3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcf\
+f3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3f\
+cff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff3fcff\
+3e3b69ecf0fe7e1fd7f3d5fe7f7e5fd79f6f97cbbfe9b7fbfebcfb7cbbfe9b7fbf8f\
+87f3f0febcfcbb7bfe9b7e3fd79f6f977fd36ff7f1febb7e9b7fbf8fc7f9f0db4f67\
+f5e7dbe5f4efdbfdf891a13f1db4f6787f3f0febf9eaff3fbf2febcfb7cbe5dff4db\
+fdff5e7dbe5dff4dbfdfc7c3f9f87f5e7e5dbdff4dbf1febcfb7cbbfe9b7fbf8ff5d\
+bf4dbfdfc7e3fcf86da7b3faf3edf2fa77edfefc48d09f8eda7b3c3f9f87f5fcf57f\
+9fdf97f5e7dbe5f2effa6dfeffaf3edf2effa6dfefe3e1fcfc3faf3f2edeffa6df8f\
+f5e7dbe5dff4dbfdfc7faedfa6dfefe3f1fe7c36d3d9fd79f6f97d3bf6ff7e24684f\
+c76d3d9e1fcfc3fafe7abfcfefcbfaf3edf2f977fd36ff7fd79f6f977fd36ff7f1f0\
+fe7e1fd79f976f7fd36fc7faf3edf2effa6dfefe3fd76fd36ff7f1f8ff3e1b69ecfe\
+bcfb7cbe9dfb7fbf1234276eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76edd\
+bb76eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76eddbb76e\
+ddbb76eddbb48d090b5fd237f086c44a23ef0e70c72b2fbb617f'
+};
 
 var test_headers = [{
   header: {
@@ -124,6 +193,37 @@ var test_header_sets = [{
 
 describe('compressor.js', function() {
   describe('HeaderTable', function() {
+  });
+
+  describe('HuffmanTable', function() {
+    describe('method encode(buffer)', function() {
+      it('should return the Huffman encoded version of the input buffer', function() {
+        var table = HuffmanTable.requestHuffmanTable;
+        for (var decoded in test_huffman_request) {
+          var encoded = test_huffman_request[decoded];
+          expect(table.encode(new Buffer(decoded)).toString('hex')).to.equal(encoded);
+        }
+        table = HuffmanTable.responseHuffmanTable;
+        for (decoded in test_huffman_response) {
+          encoded = test_huffman_response[decoded];
+          expect(table.encode(new Buffer(decoded)).toString('hex')).to.equal(encoded);
+        }
+      });
+    })
+    describe('method decode(buffer)', function() {
+      it('should return the Huffman decoded version of the input buffer', function() {
+        var table = HuffmanTable.requestHuffmanTable;
+        for (var decoded in test_huffman_request) {
+          var encoded = test_huffman_request[decoded];
+          expect(table.decode(new Buffer(encoded, 'hex')).toString()).to.equal(decoded)
+        }
+        table = HuffmanTable.responseHuffmanTable;
+        for (decoded in test_huffman_response) {
+          encoded = test_huffman_response[decoded];
+          expect(table.decode(new Buffer(encoded, 'hex')).toString()).to.equal(decoded)
+        }
+      });
+    })
   });
 
   describe('HeaderSetCompressor', function() {
@@ -255,5 +355,19 @@ describe('compressor.js', function() {
         }, 10);
       });
     });
+    describe('huffmanTable.decompress(huffmanTable.compress(buffer)) === buffer', function() {
+      it('should be true for any buffer', function() {
+        for (var i = 0; i < 10; i++) {
+          var buffer = [];
+          while (Math.random() > 0.1) {
+            buffer.push(Math.floor(Math.random() * 256))
+          }
+          buffer = new Buffer(buffer);
+          var table = HuffmanTable.requestHuffmanTable;
+          var result = table.decode(table.encode(buffer));
+          expect(result).to.deep.equal(buffer);
+        }
+      })
+    })
   });
 });
