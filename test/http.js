5ac61983f98706831693fbb36eadcac55da1c2af
Jan Schär <jscissr@gmail.com>
2017-08-29 18:41:05 +0200

    Fix http request test

--- test/http.js
+++ test/http.js
@@ -601,6 +601,7 @@ describe('http.js', function() {
             host: 'localhost',
             port: 1259,
             path: '/',
+            method: 'POST',
             ca: serverOptions.cert
           });
           request.write('Ping');
